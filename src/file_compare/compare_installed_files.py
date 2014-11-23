2a3421afab3ed97edec59d37e0fca72f6af4f3d8
Rick Broker <rbroker@xebialabs.com>
2014-11-23 18:33:01 -0500

    Switched to Ravan example

--- src/file_compare/compare_installed_files.py
+++ src/file_compare/compare_installed_files.py
@@ -1,41 +1,27 @@
 from __future__ import with_statement
 import sys
 import com.xebialabs.deployit.plugin.api.udm.Parameters
-from overtherepy import OverthereHost, OverthereHostSession, LocalConnectionOptions, OperatingSystemFamily, StringUtils
+from overtherepy import OverthereHost, OverthereHostSession, Diff, StringUtils
 
 def server_diff(hostA, hostB, source_file, remote_path):
-    global LocalConnectionOptions
     global OverthereHostSession
+    global Diff
 
     context.logOutput(remote_path)
-    localOpts = LocalConnectionOptions(os=OperatingSystemFamily.UNIX)
-    local_session =  OverthereHostSession(OverthereHost(localOpts))
     sessionA = OverthereHostSession(hostA)
     sessionB = OverthereHostSession(hostB)
     try:
-        print "HostA = %s" % (hostA)
-        print "d1 remote_file = %s" % (remote_path)
+        print "Remote file %s" % (remote_path)
         d1 = sessionA.remote_file(remote_path)
-        sfile = "hostA" + source_file
-        local_d1 = local_session.work_dir_file(sfile)
-        print "local_d1 work dir = %s" % (local_d1)
-        print "copy %s to %s" % (d1, local_d1)
-        local_session.copy_to(d1, local_d1, )
-
-        print "HostB = %s" % (hostB)
-        print "d2 remote_file = %s" % (remote_path)
         d2 = sessionB.remote_file(remote_path)
-        sfile = "hostB" + source_file
-        local_d2 = local_session.work_dir_file(sfile)
-        print "local_d2 work dir = %s" % (local_d2)
-        print "copy %s to %s" % (d2, local_d2)
-        #local_session.copy_to(d2, local_d2)
-
-        response = local_session.execute("diff %s %s" % (local_d1, local_d2), check_success=False)
+        print "Diff %s and %s" % (d1, d2)
+        diff = Diff.calculate_diff(d1, d2)
+        print str(diff.removed)
+        print str(diff.added)
+        print str(diff.changed)
     finally:
         sessionA.close_conn()
         sessionB.close_conn()
-        local_session.close_conn()
     # End try
     return response.stdout
 # End Def

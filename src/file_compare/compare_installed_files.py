e5289d37087bd15c7a50bf2c7b248a3a51a83a56
Rick Broker <rbroker@xebialabs.com>
2014-11-24 08:55:46 -0500

    Everything working after resolving local file path issue with 'diff'

--- src/file_compare/compare_installed_files.py
+++ src/file_compare/compare_installed_files.py
@@ -1,13 +1,15 @@
 from __future__ import with_statement
 import sys
 import com.xebialabs.deployit.plugin.api.udm.Parameters
-from overtherepy import OverthereHost, OperatingSystemFamily, LocalConnectionOptions, OverthereHostSession, StringUtils
+from overtherepy import OverthereHost, OperatingSystemFamily, LocalConnectionOptions, OverthereHostSession, StringUtils, Diff
 
 def server_diff(hostA, hostB, source_file, remote_path):
     global OverthereHostSession
     global OperatingSystemFamily
     global LocalConnectionOptions
     global ObertherHostSession
+    global StringUtils
+    global Diff
 
     context.logOutput(remote_path)
     localOpts = LocalConnectionOptions(os=OperatingSystemFamily.UNIX)
@@ -18,13 +20,20 @@ def server_diff(hostA, hostB, source_file, remote_path):
         d1 = sessionA.remote_file(remote_path)
         local_d1 = local_session.work_dir_file("hostA")
         local_session.copy_to(d1, local_d1)
+        myFile = "%s" % (local_d1)
+        local_f1 = myFile.split(':')[1]
 
         d2  = sessionB.remote_file(remote_path)
         local_d2 = local_session.work_dir_file("hostB")
         local_session.copy_to(d2, local_d2)
+        myFile = "%s" % (local_d2)
+        local_f2 = myFile.split(':')[1]
+
+        response = local_session.execute("diff %s %s" % (local_f1, local_f2), check_success=False)
     finally:
         sessionA.close_conn()
         sessionB.close_conn()
+        local_session.close_conn()
     # End try
     return response.stdout
 # End Def
@@ -50,4 +59,5 @@ for d in thisCi.deployeds:
             # End if
         # End if
     # End if  
+    break
 # End for

9538eca5072b39227cc2f858b38b7d621b2899a8
Rick Broker <rbroker@xebialabs.com>
2014-11-24 06:44:05 -0500

    change back to original copy plan

--- src/file_compare/compare_installed_files.py
+++ src/file_compare/compare_installed_files.py
@@ -1,24 +1,27 @@
 from __future__ import with_statement
 import sys
 import com.xebialabs.deployit.plugin.api.udm.Parameters
-from overtherepy import OverthereHost, OverthereHostSession, Diff, StringUtils
+from overtherepy import OverthereHost, OperatingSystemFamily, LocalConnectionOptions, OverthereHostSession, StringUtils
 
 def server_diff(hostA, hostB, source_file, remote_path):
     global OverthereHostSession
-    global Diff
+    global OperatingSystemFamily
+    global LocalConnectionOptions
+    global ObertherHostSession
 
     context.logOutput(remote_path)
+    localOpts = LocalConnectionOptions(os=OperatingSystemFamily.UNIX)
+    local_session =  OverthereHostSession(OverthereHost(localOpts))
     sessionA = OverthereHostSession(hostA)
     sessionB = OverthereHostSession(hostB)
     try:
-        print "Remote file %s" % (remote_path)
         d1 = sessionA.remote_file(remote_path)
-        d2 = sessionB.remote_file(remote_path)
-        print "Diff %s and %s" % (d1, d2)
-        diff = Diff.calculate_diff(d1, d2)
-        print str(diff.removed)
-        print str(diff.added)
-        print str(diff.changed)
+        local_d1 = local_session.work_dir_file("hostA")
+        local_session.copy_to(d1, local_d1)
+
+        d2  = sessionB.remote_file(remote_path)
+        local_d2 = local_session.work_dir_file("hostB")
+        local_session.copy_to(d2, local_d2)
     finally:
         sessionA.close_conn()
         sessionB.close_conn()
